52412f6065ff1b311751281b0c53e70f34c2ef59
alexander.siguenza <alexander.siguenza@gmail.com>
2021-02-11 20:47:56 -0600

    primer version

---
+++ src/edu/ujmd/guiasejercicios/ejemplos/ejercicio01.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ujmd.guiasejercicios.ejemplos;
+
+/**
+ *
+ * @author Alexander-Siguenza
+ */
+public class ejercicio01 {
+    
+      public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo Segunda Clase");
+        int sst = 0;          
+    }    
+    
+}
